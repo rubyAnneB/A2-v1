73481b4708766055ba25ace3fef3c7903d9ba2e0
Ruby <rabautista@myseneca.ca>
2020-03-20 18:04:11 -0400

    Basic capabilities implemented for entire project

--- src/seneca/btp400/A2/controllers/ThankYouScreenController.java
+++ src/seneca/btp400/A2/controllers/ThankYouScreenController.java
@@ -1,13 +1,43 @@
 package seneca.btp400.A2.controllers;
 
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 public class ThankYouScreenController implements Initializable {
+
+
+    @FXML
+    Button homebtn;
+
+    public void changeWelcomeScene (ActionEvent event) throws IOException {
+        Parent welcome = FXMLLoader.load(getClass().getResource("../resources/fxml/Welcome.fxml"));
+        Scene welcomeScene = new Scene(welcome);
+
+        //get stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(welcomeScene);
+        window.show();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
     }
+
+
 }
