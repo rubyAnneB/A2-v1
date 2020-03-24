4c7ac7f7c5be1198fa92a9d5b9ba87391401e7ba
dderich <61340085+dderich@users.noreply.github.com>
2020-03-23 20:37:30 -0400

    Update addNewVoterController.java

--- src/seneca/btp400/A2/controllers/addNewVoterController.java
+++ src/seneca/btp400/A2/controllers/addNewVoterController.java
@@ -10,12 +10,21 @@ import java.util.ResourceBundle;
  * displays form for add new voter. Takes info and puts it into the database
  */
 public class addNewVoterController  implements Initializable {
-
-
-
+    @FXML TextField fnameTyped;
+    @FXML TextField lnameTyped;
+  //@FXML TextField stnumTyped;
+    @FXML TextField emailTyped;
+    @FXML PasswordField passTyped;
+    
+    dbAccessObj db;
+    Voter voter;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        try {
+            db = new dbAccessObj();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
