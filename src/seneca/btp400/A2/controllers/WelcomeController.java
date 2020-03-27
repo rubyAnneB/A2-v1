7a5fced0f2650c5e1f36dcefbd3051e8603c54c0
dderich <61340085+dderich@users.noreply.github.com>
2020-03-27 00:01:03 -0400

    added method called "AdminLoginAction"

--- src/seneca/btp400/A2/controllers/WelcomeController.java
+++ src/seneca/btp400/A2/controllers/WelcomeController.java
@@ -16,16 +16,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * @author Ruby Anne Bautista
- * @since 2020-03-19
- * @version 1.0
- * Controller for the Welcome scene. Moves the user to the login scene
+ * @author Ruby Anne Bautista, Daniel Derich
+ * @since 2020-03-27
+ * @version 2.0
+ * Controller for the Welcome scene. Moves the user, admin to the login scene
  */
 public class WelcomeController implements Initializable {
 
     @FXML
-    Button LoginBtn;
-
+    Button LoginBtn; // User Login
+    @FXML
+    Button AdminBtn;
+    
     /**
      * Moves the user into the Login scene
      * @param event Click
@@ -42,7 +44,16 @@ public class WelcomeController implements Initializable {
         window.show();
     }
 
+    @FXML
+    private void AdminLoginAction(ActionEvent event) throws IOException {
+        Parent alogin = FXMLLoader.load(getClass().getResource("../resources/fxml/AdminLogin.fxml"));
+        Scene adminloginScene = new Scene(alogin);
 
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(adminloginScene);
+        window.show();
+    }
+    
     //add new voter- for Daniel
     //TODO: Implement method that activated upon pressing 'Add new voter'
     //This is will need new table in database that contains users with this privilege
