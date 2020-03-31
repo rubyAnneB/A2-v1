021ad8dc65a9d678a5a7cc3171b0a5490362c51f
Ruby <rabautista@myseneca.ca>
2020-03-31 15:13:47 -0400

    Converted all admin side to have navigation bar- needs to be reviewed due to inconsistent fonts and general appearance

--- src/seneca/btp400/A2/controllers/DeleteVoterController.java
+++ src/seneca/btp400/A2/controllers/DeleteVoterController.java
@@ -40,6 +40,137 @@ public class DeleteVoterController implements Initializable {
 	Administrator admin;
 	dbAccessObj db;
 
+
+	@FXML
+	Button viewCandidates;
+	@FXML
+	Button addVoterBtn;
+	@FXML
+	Button deleteVoterBtn;
+	@FXML
+	Button changePassword;
+	@FXML
+	Button logOut;
+	@FXML
+	Button addCandidatebtn;
+	@FXML
+	Button deleteCandidatebtn;
+
+	//Navigation
+	@FXML
+	private void logAdminOutScene(ActionEvent event) throws IOException{
+		admin = null;
+		Parent welcome = FXMLLoader.load(getClass().getResource("../resources/fxml/AdminLogin.fxml"));
+		Scene welcomeScene = new Scene(welcome);
+
+		//get stage information
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(welcomeScene);
+		window.show();
+	}
+
+	@FXML
+	private void addVoterScene(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/addNewVoter.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene welcomeAdminScene = new Scene(welcomeAdmin);
+
+		AddNewVoterController controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(welcomeAdminScene);
+		window.show();
+	}
+	@FXML
+	private void deleteVoterScene (ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/DeleteVoter.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene welcomeAdminScene = new Scene(welcomeAdmin);
+
+		DeleteVoterController controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(welcomeAdminScene);
+		window.show();
+	}
+
+	@FXML
+	private void getVotingResultsScene(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/ViewCandidateResults.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene welcomeAdminScene = new Scene(welcomeAdmin);
+
+		ViewCandidateResults controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(welcomeAdminScene);
+		window.show();
+	}
+
+	@FXML
+	private void passwordChScene(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/AdminChangePassword.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene scene = new Scene(welcomeAdmin);
+
+		AdminChangePassword controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+
+	@FXML
+	private void addCandidateScene(ActionEvent event) throws IOException{
+		//link to addCandidate
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/AddCandidate.fxml"));
+		Parent addC = loader.load();
+		Scene scene = new Scene(addC);
+
+		AddCandidateController controller = loader.getController();
+		controller.initData(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+
+	@FXML
+	private void deleteCandidateScene(ActionEvent event)throws IOException{
+		//link to deleteCandidate
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/DeleteCandidate.fxml"));
+		Parent deleteC = loader.load();
+		Scene scene = new Scene(deleteC);
+
+		DeleteCandidateController controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+
+
 	public void setAdmin(Administrator admin) {
     	this.admin = admin;
     }
--- src/seneca/btp400/A2/controllers/ViewCandidateResults.java
+++ src/seneca/btp400/A2/controllers/ViewCandidateResults.java
@@ -39,6 +39,139 @@ public class ViewCandidateResults implements Initializable {
 	Administrator admin;
 
 
+
+	@FXML
+	Button viewCandidates;
+	@FXML
+	Button addVoterBtn;
+	@FXML
+	Button deleteVoterBtn;
+	@FXML
+	Button changePassword;
+	@FXML
+	Button logOut;
+	@FXML
+	Button addCandidatebtn;
+	@FXML
+	Button deleteCandidatebtn;
+
+	//Navigation
+	@FXML
+	private void logAdminOutScene(ActionEvent event) throws IOException{
+		admin = null;
+		Parent welcome = FXMLLoader.load(getClass().getResource("../resources/fxml/AdminLogin.fxml"));
+		Scene welcomeScene = new Scene(welcome);
+
+		//get stage information
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(welcomeScene);
+		window.show();
+	}
+
+	@FXML
+	private void addVoterScene(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/addNewVoter.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene welcomeAdminScene = new Scene(welcomeAdmin);
+
+		AddNewVoterController controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(welcomeAdminScene);
+		window.show();
+	}
+	@FXML
+	private void deleteVoterScene (ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/DeleteVoter.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene welcomeAdminScene = new Scene(welcomeAdmin);
+
+		DeleteVoterController controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(welcomeAdminScene);
+		window.show();
+	}
+
+	@FXML
+	private void getVotingResultsScene(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/ViewCandidateResults.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene welcomeAdminScene = new Scene(welcomeAdmin);
+
+		ViewCandidateResults controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(welcomeAdminScene);
+		window.show();
+	}
+
+	@FXML
+	private void passwordChScene(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/AdminChangePassword.fxml"));
+		Parent welcomeAdmin = loader.load();
+
+		Scene scene = new Scene(welcomeAdmin);
+
+		AdminChangePassword controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+
+	@FXML
+	private void addCandidateScene(ActionEvent event) throws IOException{
+		//link to addCandidate
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/AddCandidate.fxml"));
+		Parent addC = loader.load();
+		Scene scene = new Scene(addC);
+
+		AddCandidateController controller = loader.getController();
+		controller.initData(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+
+	@FXML
+	private void deleteCandidateScene(ActionEvent event)throws IOException{
+		//link to deleteCandidate
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../resources/fxml/DeleteCandidate.fxml"));
+		Parent deleteC = loader.load();
+		Scene scene = new Scene(deleteC);
+
+		DeleteCandidateController controller = loader.getController();
+		controller.setAdmin(admin);
+		admin = null;
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+
+
+
+
 	public void setAdmin(Administrator admin) {
 		this.admin = admin;
 	}
@@ -74,7 +207,7 @@ public class ViewCandidateResults implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		candidateData = new XYChart.Series<>();
-		nameAxis.setTickLabelRotation(0);
+
 		try {
 			db = new dbAccessObj();
 		} catch (SQLException e) {
@@ -86,7 +219,6 @@ public class ViewCandidateResults implements Initializable {
 			e.printStackTrace();
 		}
 
-		voteAxis.setTickLabelRotation(90);
 
 	}
 
